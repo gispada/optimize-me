7af36726d5694335ff57977f87295595d21242b2
Gianluca Spada <gianluca.spada@stackhouse.it>
2022-01-15 14:23:56 +0100

    Simple home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Home = () => {
-  return <Typography>Hello world</Typography>
+  return (
+    <Box>
+      <Typography fontSize="large" paragraph>
+        This application is <strong>intentionally</strong> coded in a way that
+        ignores React best practices on optimization.
+      </Typography>
+      <Typography fontSize="large" paragraph>
+        As a result, the three main sections &#8212; Registration, Gallery and
+        Albums' photos &#8212; feel slow when interacting with them, with
+        noticeable FPS drops especially when typing.
+      </Typography>
+    </Box>
+  )
 }
 
 export default Home
